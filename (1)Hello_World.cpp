3835768fee9853eb5ca177252b7f311fcfac7d8a
mass9 <30389597+mass9@users.noreply.github.com>
2017-11-27 20:32:59 +0530

    Add files via upload

---
+++ (1)Hello_World.cpp
@@ -0,0 +1,13 @@
+#include <iostream>   //Hearder file for input and output
+
+using namespace std;  //This line simply means the use of Standard Library
+
+int main()            //Main function is used to complie and Excute code
+
+{                     //Brackets are necessary to limit upto which lines complier should compile 
+
+cout << "Hello World"; //Cout is used ti display output
+
+return 0;              //Returnig no value to main  
+
+}
